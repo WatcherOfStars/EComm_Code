fb3c708daea1739d90a4d85cb32a461832cd0f75
Owen Anderson <owencanderson23@gmail.com>
2026-01-21 15:45:12 -0500

    changes

--- ft.py
+++ ft.py
@@ -3,32 +3,30 @@ import matplotlib
 matplotlib.use('QtAgg') 
 import matplotlib.pyplot as plt
 
-# --- 1. Signal Parameters ---
+# Params
 Fs = 1000  # Sampling frequency (samples per second)
 T = 1.0    # Total time duration (seconds)
 N = int(Fs * T) # Number of samples
 t = np.linspace(0.0, T, N, endpoint=False) # Time vector
 
-# --- 2. Create a Signal ---
-# A signal composed of two sine waves at 50 Hz and 120 Hz
+# Create the signal
 freq1 = 50.0
 freq2 = 120.0
 signal = np.sin(2 * np.pi * freq1 * t) + 0.5 * np.sin(2 * np.pi * freq2 * t) #
 
-# --- 3. Compute the FFT ---
+# Compute fft
 # np.fft.fft computes the 1D DFT
 yf = np.fft.fft(signal)
 
 # Normalize the result by the number of samples
 yf = yf / N
 
-# --- 4. Get the Frequency Axis ---
+# Get frequency axis
 # np.fft.fftfreq returns the frequency bins corresponding to the output of fft()
 xf = np.fft.fftfreq(N, 1/Fs)
 
-# --- 5. Plotting ---
 
-# We only plot the positive frequencies for a real-valued signal, as the spectrum is symmetric
+# Only plot the positive frequencies for a real-valued signal, as the spectrum is symmetric
 # We use slicing to get the first half of the frequency and amplitude data
 xf_pos = xf[:N//2]
 yf_pos = np.abs(yf[:N//2]) # np.abs gives the magnitude (amplitude)
